46c431d8fa7fac75446dfe25911415297888e4e0
saemipp <101797498+saemipp@users.noreply.github.com>
2023-09-29 01:13:21 +0200

    Added file parsing into ndarrays. Using np.fromfile(bin) and readlines(ascii)

--- filereader.py
+++ filereader.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 import struct
 import time
+import numpy as np
 
 path = 'test_stl_ascii.stl' #Filename definition
 
@@ -26,6 +27,16 @@ def openSTL(path: 'str'):
             return f'STL-Format error in line {self.line}. File ends unexpectedly. File does not comply with STL format standards.'
 
     def checkParseAsciiSTL(file: 'TextIOWrapper'):
+
+        lines = 0
+        curr_triangle = 1
+        for line in file:
+            lines += 1
+
+        num_triangles = int((lines-2)/7)
+
+        triangles = np.zeros([num_triangles,12],dtype=float)
+
         linenr = 1
         file.seek(0)
         if file.readline().rsplit(' ')[0] != ('solid'):
@@ -41,7 +52,7 @@ def openSTL(path: 'str'):
             if line[0:2] != ['facet','normal'] or len(line[2:]) != 3:
                 raise STLFormatError(linenr, '"facet normal X Y Z"')
             try:
-                list(map(float, line[2:]))
+                triangles[curr_triangle-1,0:3] = line[2:]
             except ValueError:
                 print('Normal Vector Values are not formatted Correctly. Error in STL-Line ' + str(linenr))
             
@@ -60,7 +71,7 @@ def openSTL(path: 'str'):
                 if line[0:1] != ['vertex'] or len(line[1:]) != 3:
                     raise STLFormatError(linenr,'"vertex X Y Z"')
                 try:
-                    list(map(float, line[1:]))
+                    triangles[curr_triangle-1,3+3*i:6+3*i] = line[1:]
                 except ValueError:
                     print('Vertex not formatted correctly. Error in STL-Line ' + str(linenr))
             
@@ -68,15 +79,17 @@ def openSTL(path: 'str'):
             if line == ['']:
                 raise STLEndingError('File ends abruptly in STL-Line ' + str(linenr))
             linenr += 1
-            if line[0:2] != ['endloop']:
+            if line != ['endloop']:
                 raise STLFormatError(linenr,'"endloop"')
             
             line = " ".join(file.readline().strip().replace('\n','').replace('\t','').split()).rsplit(' ')
             if line == ['']:
                 raise STLEndingError(linenr)
             linenr += 1
-            if line[0:2] != ['endfacet']:
+            if line != ['endfacet']:
                 raise STLFormatError(linenr,'"endfacet"')
+            curr_triangle += 1
+        return triangles
 
     def checkParseBinSTL(file: 'TextIOWrapper'):
         file.seek(80)
@@ -84,6 +97,11 @@ def openSTL(path: 'str'):
         file.read()
         if file.tell() != (80+4+50*num_triangles):
             raise STLFormatError(-1)
+        file.seek(0)
+        triangles = np.fromfile(file,dtype=[('TriangleData','12<f4'),('AttributeByteCount','<u2')],offset=84)['TriangleData'].astype(float)
+        print(triangles[:,2])
+        return triangles
+
 
     if file_path.endswith('.stl'): #Check if the given File is of Filetype STL
         try:
@@ -93,16 +111,17 @@ def openSTL(path: 'str'):
                 isAscii = True
                 bin_file.close()                #reopens File in ASCII read mode
                 ascii_file = open(file_path,'r')    #reopens File in ASCII read mode
-                checkParseAsciiSTL(ascii_file)
+                triangles = checkParseAsciiSTL(ascii_file)
                 ascii_file.close()
                 print('ASCII file valid at:', file_path)
-                return ascii_file
+                return triangles
             else:
                 print('Found Binary File at:', file_path)
                 isBinary = True
-                checkParseBinSTL(bin_file)
+                triangles = checkParseBinSTL(bin_file)
                 print('Binary file valid at:', file_path)
                 bin_file.close()
+                return triangles
         except OSError as e:
             print(e)
         except STLEndingError as e:
