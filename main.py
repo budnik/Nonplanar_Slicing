852a8bb7d006dd328227f9d3b878309d97200d7b
Severin <zuercse1@students.zhaw.ch>
2023-10-25 22:28:18 +0200

    Fall 1 working, fr -> added Z line to array

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Fall = 0
     
     orig_stl_path = 'Welle.stl'
-    prusa_config_path = 'config.ini'
+    prusa_config_path = 'test_files/generic_config_Deltiq2.ini'
     orig_stl = fr.openSTL(orig_stl_path)
     filtered_surface = sf.create_surface(orig_stl,np.deg2rad(45))
     z_mean = np.average(filtered_surface[:,2])
