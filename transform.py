74ea7e647c70bed78e578ff6d38a871c8aeeb3be
saemipp <101797498+saemipp@users.noreply.github.com>
2023-10-27 12:19:31 +0200

    added planing of first layer and corrected e axis

--- transform.py
+++ transform.py
@@ -1,8 +1,10 @@
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 import os
 import surface as sf
 from filereader import gcode_dtype
+import filereader as fr
 
 
 # Transforms a given stl file down, so that the top of the part is plane
@@ -31,9 +33,9 @@ def projectSTL(stl_data: 'np.ndarray', filtered_surface: 'np.ndarray', method='i
     else:
         raise ValueError("Method mus be \"interpolate\" or \"mirror\"")
 
-def transformGCODE(gcode_data: 'gcode_dtype',stl_path: 'str', filtered_surface: 'np.ndarray'):
-    gcode_file = gcode_writer(stl_path)
-    z_min = 0
+def transformGCODE(gcode_data: 'gcode_dtype',stl_path: 'str', filtered_surface: 'np.ndarray', prusa_generated_config):
+    gcode_file = gcode_writer('temp_gcode.gcode')
+    z_lowest = 0
     x = 0
     y = 0
     z = 0
@@ -47,21 +49,42 @@ def transformGCODE(gcode_data: 'gcode_dtype',stl_path: 'str', filtered_surface: 
             if ~np.isnan(row['X']): x = row['X'] 
             if ~np.isnan(row['Y']): y = row['Y'] 
             if ~np.isnan(row['Z']): z = row['Z'] 
-            #e_new = np.nan
-            #f_new = np.nan
             length = np.round(np.sqrt((x-x_old)**2 + (y-y_old)**2 + (z-z_old)**2),decimals=0)
             f_new = row['F']
-            e_new = row['E']/length
+            if (length != 0 and row['E'] != np.nan):
+                e_new = row['E']/length
+            elif (length<1.0):
+                gcode_file.set_line('G1',x,y,z,row['E'],row['F'])
+            elif row['E'] == np.nan:
+                e_new = np.nan
+            else:
+                e_new = row['E']
             for i in range(1,int(length)+1):
                 x_new = x_old + (x-x_old)/length*i
                 y_new = y_old + (y-y_old)/length*i
                 z_new = z_old + (z-z_old)/length*i
-                zTrans = z_new + sf.interpolate_grid(np.array([[x_new,y_new]]),filtered_surface)
-                if zTrans < z_min: zTrans = z_min
-                gcode_file.set_line('G1',x_new,y_new,zTrans,e_new,f_new)
-        print(j)
+                if z_new < 0 : z_new = 0
+                gcode_file.set_line('G1',x_new,y_new,z_new,e_new,f_new)
+        row_old = row
     gcode_file.flush()
     gcode_file.stop()
+
+    layer_height = 0.2
+
+    gcode_file2 = gcode_writer(stl_path)
+    gcode_temp = fr.openGCODE_keepcoms('temp_gcode.gcode', get_config=False)
+    gcode_temp['Z'][~np.isnan(gcode_temp['Z'])] = gcode_temp['Z'][~np.isnan(gcode_temp['Z'])] + scipy.interpolate.griddata(filtered_surface[:,:2], filtered_surface[:,2], (gcode_temp['X'][~np.isnan(gcode_temp['Z'])], gcode_temp['Y'][~np.isnan(gcode_temp['Z'])]), 'linear', fill_value = np.inf)
+    gcode_temp['Z'][np.isinf(gcode_temp['Z'])] = gcode_temp['Z'][np.isinf(gcode_temp['Z'])] + scipy.interpolate.griddata(filtered_surface[:,:2], filtered_surface[:,2], (gcode_temp['X'][np.isinf(gcode_temp['Z'])], gcode_temp['Y'][np.isinf(gcode_temp['Z'])]), 'nearest')
+    z_lowest = np.min(gcode_temp['Z'][~np.isnan(gcode_temp['E'])])
+    print('lowest z = ', z_lowest)
+    gcode_temp['Z'] = gcode_temp['Z'] - z_lowest + layer_height
+    #gcode_temp['E'][np.less_equal(gcode_temp['Z'],0.1)] *= 0.75
+    gcode_temp['Z'][np.less_equal(gcode_temp['Z'],2*layer_height)] = layer_height
+    for line in gcode_temp:
+        gcode_file2.set_line(*line)
+    gcode_file2.flush()
+    gcode_file2.set_comm(prusa_generated_config)
+    gcode_file2.stop()
 
 class gcode_writer:
     def __init__(self, path, batch_size = 500):
@@ -83,18 +106,26 @@ class gcode_writer:
         if(self.currline >= self.batchsize):
             self.flush()
 
+    def set_comm(self, comment):
+        try:
+            self.f.write(comment.decode('utf-8') )
+        except AttributeError:
+            self.f.write(comment)
+        except Exception as e:
+            raise e
+
     def flush(self):
         for line in self.lines[0:self.currline]:
             if line['Instruction'] == 'G1':
-                X = '' if np.isnan(line['X']) else (' X' + str(line['X']))
-                Y = '' if np.isnan(line['Y']) else (' Y' + str(line['Y']))
-                Z = '' if np.isnan(line['Z']) else (' Z' + str(line['Z']))
-                E = '' if np.isnan(line['E']) else (' E' + str(line['E']))
-                F = '' if np.isnan(line['F']) else (' F' + str(line['F']))
+                X = '' if np.isnan(line['X']) else (' X' + str(np.round(line['X'],4)))
+                Y = '' if np.isnan(line['Y']) else (' Y' + str(np.round(line['Y'],4)))
+                Z = '' if np.isnan(line['Z']) else (' Z' + str(np.round(line['Z'],4)))
+                E = '' if np.isnan(line['E']) else (' E' + str(np.round(line['E'],4)))
+                F = '' if np.isnan(line['F']) else (' F' + str(np.round(line['F'],0)))
                 self.f.write(f"G1{X}{Y}{Z}{E}{F}\n")
                 pass
             else:
-                self.f.write(str(line['Instruction']) + '\n')
+                self.f.write(str(line['Instruction'] + '\n'))
                 pass
         self.currline = 0
 
