073d9a74f5e3cf37aa25b341ebd856f2918a5b2b
Severin <zuercse1@students.zhaw.ch>
2023-11-01 16:46:50 +0100

    added limits

--- temp_main_2.py
+++ temp_main_2.py
@@ -5,14 +5,14 @@ import gcode_transform_1
 
 stl_pfad = "test_files/test_pa_outline_fein_2.stl"
 triangle_array = filereader.openSTL(stl_pfad)
-Oberflaeche = surface.create_surface(triangle_array, np.pi / 3) # Winkel
+Oberflaeche, limits = surface.create_surface(triangle_array, np.pi / 3) # Winkel
 
 path_gcode = "C:/Users/zuerc/Documents/Informatik_Projekte/PA/PA23_wuem_346_Nonplanar/output.gcode"
 gcode_raw, config = filereader.openGCODE_keepcoms(path_gcode, get_config=True)
 
+print(limits)
 
-
-gcode_transform_1.trans_gcode(gcode_raw, Oberflaeche, config_string=config)
+gcode_transform_1.trans_gcode(gcode_raw, Oberflaeche, limits, config_string=config)
 
 # -> x, y und z vektoren mit den zugehoerigen e und f- Werten in ein Array schreiben
 # -> Offset des GCodes erkennen, resp. bei X und Y = 0 slicen?
